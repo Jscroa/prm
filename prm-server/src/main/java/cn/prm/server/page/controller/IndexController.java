768319f6a7eb86cabdaa4fdc35c5402a40a42164
yaoyao <yyao.sss8@gmail.com>
2016-07-11 15:49:39 +0800

    去掉没必要的needlogin注解

--- prm-server/src/main/java/cn/prm/server/page/controller/IndexController.java
+++ prm-server/src/main/java/cn/prm/server/page/controller/IndexController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import cn.prm.server.annotation.NeedLogin;
 import cn.prm.server.bean.CurrUser;
 import cn.prm.server.commons.BaseController;
 import cn.prm.server.commons.MAVHelper;
